4fa07913c7cffabd67e68c16d54e4e895a6cb437
alexwitt2399 <awitt2399@utexas.edu>
2020-10-17 16:37:46 -0500

    more cpp work

--- WORKSPACE
+++ WORKSPACE
@@ -1,5 +1,13 @@
 load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
 
+
+http_archive(
+    name = "com_google_absl",
+    sha256 = "808350c4d7238315717749bab0067a1acd208023d41eaf0c7360f29cc8bc8f21",
+    strip_prefix = "abseil-cpp-20200923.1",
+    urls = ["https://github.com/abseil/abseil-cpp/archive/20200923.1.tar.gz"],
+)
+
 http_archive(
     name = "com_google_logging",
     sha256 = "9e1b54eb2782f53cd8af107ecf08d2ab64b8d0dc2b7f5594472f3bd63ca85cdc",
@@ -19,8 +27,8 @@ http_archive(
 
 http_archive(
     name = "eigen",
-    build_file = "//third_party/eigen",
+    build_file = "//third_party/eigen:BUILD",
     sha256 = "146a480b8ed1fb6ac7cd33fec9eb5e8f8f62c3683b3f850094d9d5c35a92419a",
-    url = "https://gitlab.com/libeigen/eigen/-/archive/3.3.8/eigen-3.3.8.tar.gz",
-    strip_prefix="eigen-eigen-c5e90d9e764e"
+    url = "https://github.com/alexwitt2399/spring_mass_solver/releases/download/v0.0.1-alpha/eigen-3.3.8.tar.gz",
+    strip_prefix="eigen-3.3.8"
 )
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,21 +1,57 @@
 // Author: Alex Witt <awitt2399@utexas.edu>
 //
 
-#include <Egien
+#include <iostream>
+#include <string>
+
+#include "absl/strings/str_split.h"
+#include "Eigen/Dense"
 #include "glog/logging.h"
 
-DEFINE_int32(num_masses, 3, "Number of masses in the system.");
-DEFINE_int32(num_springs, 4, "Number of springs in the system.");
 DEFINE_string(spring_constants, "1,1,1,1", "The spring constants.");
 DEFINE_string(masses, "1,1,1,1", "The masses of the weights.");
 
 
-std::vector<std::vector<float>> 
+Eigen::MatrixXd GetDifferenceMatrix(
+    const int rows, const int cols, const bool diagonal)
+{
+    auto A = Eigen::MatrixXd(rows, cols);
+    A.setZero();
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (i == j)
+            {
+                A(j , i) = -1.0;
+            }
+            else if (i - j == diagonal + 1)
+            {
+               A(j , i) = 1.0; 
+            }
+        }
+    }
+    return A;
+}
 
 
 int main(int argc, char** argv)
 {
-
     google::InitGoogleLogging(argv[0]);
+
+
+    std::vector<float> spring_constants;
+    for (const auto& s : absl::StrSplit(FLAGS_spring_constants, ","))
+    {
+        spring_constants.push_back(std::stof(std::string(s), nullptr));
+    }
+
+    std::vector<float> masses;
+    for (const auto& s : absl::StrSplit(FLAGS_masses, ","))
+    {
+        masses.push_back(std::stof(std::string(s), nullptr));
+    }  
+
+    auto A = GetDifferenceMatrix(
+        spring_constants.size(), masses.size(), !(spring_constants.size() >= masses.size()));
+
     return 0;
 }
